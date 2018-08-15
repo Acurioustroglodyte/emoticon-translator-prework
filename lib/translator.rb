ed4c787fe869613f6a6bc2ae362c35a5aec0255b
Trevor Jameson <tjameson15@gmail.com>
2018-08-15 15:03:35 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 require "yaml"
 require 'pry'
-emoticons = YAML.load_file('emoticons.yml')
+emoticons = YAML.load_file('./emoticons.yml')
 
 def load_library
   # code goes here
