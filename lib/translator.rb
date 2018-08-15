27efca28911d68ad971b56823734ce63c60a101d
Trevor Jameson <tjameson15@gmail.com>
2018-08-15 15:20:40 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,6 @@ def load_library(file_path)
   get_emoticons = {}
   get_emoticons["get_emoticon"] = emoticons.values
   get_emoticons["get_meaning"] = emoticons.keys
-  binding.pry
 end
 
 def get_japanese_emoticon
